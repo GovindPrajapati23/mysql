db1e85ad027eb951dd7c1b27290559223a37518f
Govind Prajapati <govindkumar76299@gmail.com>
2024-03-25 18:13:14 +0530

    Added basic for Mysql

--- lession1.sql
+++ lession1.sql
@@ -2,4 +2,5 @@ create database college;
 use college;
 create table student (id int primary key,name varchar(100));
 show tables;
-
+insert into student values (1, 'Baba'),(2,'Raj');
+select * from student;
